cfa8edc5a0320b8fd8a7991d773c3337c9ff5a39
Fabio Cezar Baía <fabio.baia@benner.com.br>
2015-02-27 16:24:59 -0300

    adicionado botao Hello

--- GitTutorial/GitTutorial/Form1.Designer.cs
+++ GitTutorial/GitTutorial/Form1.Designer.cs
@@ -43,23 +43,15 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "GitTutorial";
             // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(244, 239);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(28, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "v0.1";
-            // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 226);
+            this.button1.Location = new System.Drawing.Point(13, 74);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Hello";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
